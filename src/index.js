9c1e7d587f19f3bb68257e2366394b39ecadf663
AntonVoinilovich <anton.voinilovich@gmail.com>
2021-03-16 02:33:41 +0300

    fix: add full task TypArrProblem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length && array.length) {
+    let minElement = array[0];
+    array.forEach(element => {
+      if (element <= minElement) {
+        minElement = element;
+      };
+    });
+    return minElement;
+  } else {
+    return 0;
+  };
 }
-
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length && array.length) {
+    let maxElement = array[0];
+    array.forEach(element => {
+      if (element >= maxElement) {
+        maxElement = element;
+      };
+    });
+    return maxElement;
+  } else {
+    return 0;
+  };
 }
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length && array.length) {
+    let sum = 0;
+    array.forEach(element => {
+      sum += element;
+    });
+    return sum / array.length;
+  } else {
+    return 0;
+  };
 }
